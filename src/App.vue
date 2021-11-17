a311527e9f1922179a4184dd0d0de96ae55e39b1
AntoBB <antolatella@gmail.com>
2021-11-17 22:41:50 +0100

    Basic Example with Login

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer 
+    v-model="drawer"
+    app
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Ndo Menu
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Lorem Ipsum
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>NDO Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({ 
+      drawer: null,
+      items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard', to: '/' },
+          { title: 'Login', icon: 'mdi-account', to: '/Login'},
+          { title: 'About', icon: 'mdi-help-box', to: '/About' },
+          { title: 'Some Stuff', icon: 'mdi-alert-octagram', to: '/Stuff'}
+        ] }),
+  }
+</script>
