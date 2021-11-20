2bbfd2ca637b8ec573ccf03da2b619da0c2501b0
AntoBB <antolatella@gmail.com>
2021-11-20 23:19:21 +0100

    Map page new vers

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,31 +1,75 @@
 <template>
-<GmapMap
-  :center="{lat:44.10, lng:12.51}"
-  :zoom="7"
-  map-type-id="hybrid"
-  style="width: 1280px; height: 650px"
->
-  <GmapMarker
-    :key="index"
-    v-for="(m, index) in markers"
-    :position="m.position"
-    :clickable="true"
-    :draggable="true"
-    @click="center=m.position"
-  />
-</GmapMap>
+  <div>
+    <div>
+      <h2>Vue Js Search and Add Marker</h2>
+
+      <label>
+        <gmap-autocomplete @place_changed="initMarker"></gmap-autocomplete>
+
+        <button @click="addLocationMarker">Add</button>
+      </label>
+      <br/>
+ 
+    </div>
+    <br>
+    <gmap-map
+        :zoom="14"    
+        :center="center"
+        style="width:100%;  height: 600px;"
+      >
+      <gmap-marker
+        :key="index"
+        v-for="(m, index) in locationMarkers"
+        :position="m.position"
+        @click="center=m.position"
+      ></gmap-marker>
+    </gmap-map>
+  </div>
 </template>
-
+ 
 <script>
-  export default {
-    mounted () {
-      // At this point, the child GmapMap has been mounted, but
-      // its map has not been initialized.
-      // Therefore we need to write mapRef.$mapPromise.then(() => ...)
-
-      this.$refs.mapRef.$mapPromise.then((map) => {
-        map.panTo({lat: 1.38, lng: 103.80})
-      })
+export default {
+  name: "AddGoogleMap",
+  data() {
+    return {
+      center: { 
+        lat: 39.7837304,
+        lng: -100.4458825
+      },
+      locationMarkers: [],
+      locPlaces: [],
+      existingPlace: null
+    };
+  },
+ 
+  mounted() {
+    this.locateGeoLocation();
+  },
+ 
+  methods: {
+    initMarker(loc) {
+      this.existingPlace = loc;
+    },
+    addLocationMarker() {
+      if (this.existingPlace) {
+        const marker = {
+          lat: this.existingPlace.geometry.location.lat(),
+          lng: this.existingPlace.geometry.location.lng()
+        };
+        this.locationMarkers.push({ position: marker });
+        this.locPlaces.push(this.existingPlace);
+        this.center = marker;
+        this.existingPlace = null;
+      }
+    },
+    locateGeoLocation: function() {
+      navigator.geolocation.getCurrentPosition(res => {
+        this.center = {
+          lat: res.coords.latitude,
+          lng: res.coords.longitude
+        };
+      });
     }
   }
+};
 </script>
