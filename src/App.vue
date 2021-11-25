49fe50978cb3bf1028f114d446b473b8140bcc74
AntoBB <antolatella@gmail.com>
2021-11-25 22:26:41 +0100

    added avatar in navbar-app

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
     v-model="drawer"
     app
     >
+    
       <v-list-item>
+        
         <v-list-item-content>
           <v-list-item-title class="text-h6">
             Ndo Menu
@@ -40,8 +42,61 @@
 
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
       <v-toolbar-title>NDO Application</v-toolbar-title>
+      <v-spacer></v-spacer>
+            <v-menu
+        bottom
+        min-width="200px"
+        rounded
+        offset-y
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn
+            icon
+            x-large
+            v-on="on"
+          >
+            <v-avatar
+              color="brown"
+              size="48"
+            >
+              <span class="white--text text-h5">{{ user.initials }}</span>
+            </v-avatar>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-list-item-content class="justify-center">
+            <div class="mx-auto text-center">
+              <v-avatar
+                color="brown"
+              >
+                <span class="white--text text-h5">{{ user.initials }}</span>
+              </v-avatar>
+              <h3>{{ user.fullName }}</h3>
+              <p class="text-caption mt-1">
+                {{ user.email }}
+              </p>
+              <v-divider class="my-3"></v-divider>
+              <v-btn 
+              v-on:click="openLoginPage()"
+                depressed
+                rounded
+                text
+              >
+                Login
+              </v-btn>
+              <v-divider class="my-3"></v-divider>
+              <!--<v-btn
+                depressed
+                rounded
+                text
+              >
+                Disconnect
+              </v-btn>-->
+            </div>
+          </v-list-item-content>
+        </v-card>
+      </v-menu>
     </v-app-bar>
 
     <v-main>
@@ -51,8 +106,17 @@
 </template>
 
 <script>
+import About from './views/About.vue'
   export default {
     data: () => ({ 
+      user: {
+        initials: 'O',
+        fullName: 'Ospite',
+        //email: 'john.doe@doe.com',
+      },
+      //components: {
+      //  'about': About,
+      //},
       drawer: null,
       items: [
           { title: 'Dashboard', icon: 'mdi-view-dashboard', to: '/' },
@@ -61,5 +125,11 @@
           { title: 'About', icon: 'mdi-help-box', to: '/About' },
           { title: 'Some Stuff', icon: 'mdi-alert-octagram', to: '/Stuff'}
         ] }),
+        methods: {
+          async openLoginPage() {
+            this.$router.push('/Login');
+            //this.$router.push({name:'home', params: {id: '[paramdata]'}}); 
+          }
+        }
   }
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,98 +1,68 @@
+<!--https://vuetifyjs.com/en/components/avatars/#advanced-usage -->
 <template>
-  <div class="container">
-  <h1>Latest Posts</h1>
-  <!-- Create Post Here -->
-  <div class="create-post">
-    <label for="create-post">Say Something...</label>
-    <input type="text" id="create-post" v-model="text" placeholder="Create a post">
-    <button v-on:click="createPost">Post! </button>
-  </div>
-  <hr>
-  <p class = "error" v-if="error">{{ error }}</p>
-  <div class="posts-container">
-    <div class="post"
-    v-for="(post, index) in posts"
-    v-bind:item="post"
-    v-bind:index="index"
-    v-bind:key="post._id"
-    v-on:dblclick="deletePost(post._id)"
-    >
-      {{ `${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()}`}}
-      <p class="text">{{ post.text }}</p>
-    </div>
-  </div>
-  </div>
+
+      <v-menu
+        bottom
+        min-width="200px"
+        rounded
+        offset-y
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn
+            icon
+            x-large
+            v-on="on"
+          >
+            <v-avatar
+              color="brown"
+              size="48"
+            >
+              <span class="white--text text-h5">{{ user.initials }}</span>
+            </v-avatar>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-list-item-content class="justify-center">
+            <div class="mx-auto text-center">
+              <v-avatar
+                color="brown"
+              >
+                <span class="white--text text-h5">{{ user.initials }}</span>
+              </v-avatar>
+              <h3>{{ user.fullName }}</h3>
+              <p class="text-caption mt-1">
+                {{ user.email }}
+              </p>
+              <v-divider class="my-3"></v-divider>
+              <v-btn
+                depressed
+                rounded
+                text
+              >
+                Edit Account
+              </v-btn>
+              <v-divider class="my-3"></v-divider>
+              <v-btn
+                depressed
+                rounded
+                text
+              >
+                Disconnect
+              </v-btn>
+            </div>
+          </v-list-item-content>
+        </v-card>
+      </v-menu>
 </template>
 
 <script>
-import PostService from '../PostService'
-
-export default {
-  name: 'PostComponent',
-  data(){
-    return {
-      posts: [],
-      error: '',
-      text: ''
-    }
-  },
-  async created() {
-    try {
-      this.posts = await PostService.getPosts();
-    } catch(err){
-      this.error = err.message;
-    }
-  },
-  methods: {
-    async createPost() {
-      await PostService.insertPost(this.text);
-      this.posts = await PostService.getPosts();
-    },
-    async deletePost(id) {
-      await PostService.deletePost("/"+id);
-      this.posts = await PostService.getPosts();
-    }
+  export default {
+    data: () => ({
+      user: {
+        initials: 'JD',
+        fullName: 'John Doe',
+        email: 'john.doe@doe.com',
+      },
+    }),
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-div.container {
-  max-width: 800px;
-  margin: 0 auto;
-}
-
-p.error {
-  border: 1px solid #ff5b5f;
-  background-color: #ffc5c1;
-  padding:  10px;
-  margin-bottom: 15px;
-}
-
-div.post {
-  position: relative;
-  border: 1px solid #5bd658;
-  background-color:  #bcffb8;
-  padding: 10px 10px 30px 10px;
-  margin-bottom: 15px;
-}
-
-div.created-at {
-  position: absolute;
-  top: 0;
-  left: 0;
-  padding: 5px 15px 5px 15px;
-  background-color: darkgreen;
-  color: white;
-  font-size: 13px;
-}
-
-p.text {
-  font-size: 22px;
-  font-weight: 700;
-  margin-bottom: 0;
-}
-</style>
-
-
